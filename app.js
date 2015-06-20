38ad6a1f2a389560fa7b1fe45773d586abe232a5
Qianglong Mo <sammok2003@hotmail.com>
2015-06-20 20:14:23 +0800

    Add nodejs

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    app = express();
+
+app.get('/', function(req, res){
+    res.redirect('http://www.sisobrand.com');
+    res.status(200).sendFile('index.html');
+});
+
+app.get('*', function(req, res){
+    res.status(404).send('Sorry, we cannot find that!');
+});
+
+app.listen(1111, "120.26.48.94");
+console.log('Express server started at 120.26.48.94:1111');
+
