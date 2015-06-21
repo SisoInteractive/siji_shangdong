bc4472a270f8bc140d500c5c8efd588ed5a5c943
Qianglong Mo <sammok2003@hotmail.com>
2015-06-21 11:55:25 +0800

    Deploy.

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -315,7 +315,7 @@ var app = {
 
         function submitUserInfo () {
             //  test is user's name and phone number valid
-            var phoneValid = /^0?(13[0-9]|15[0-9]| 17[0-9] |18[0-9]|14[57])[0-9]{8}$/.test($('.form-number').val());
+            var phoneValid = /^0?(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[57])[0-9]{8}$/.test($('.form-number').val());
             if ($('.form-name').val().trim()) {
 
                 if (phoneValid) {
